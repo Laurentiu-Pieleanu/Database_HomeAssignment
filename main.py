29f46b3bdb76295613ecc3b515076c8a83761cef
Laurentiu-Pieleanu <laurentiu.pieleanu.f32520@mcast.edu.mt>
2025-04-09 20:39:36 +0200

    Step 4 - Task 3
    Added the '@app.get' for the sprites, audios and scores for Task 3
    In order to be able to use postman and GET the data from the MongoDB Atlas database

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI, File, UploadFile, HTTPException 
-from pydantic import BaseModel 
+from pydantic import BaseModel
+from bson import ObjectId
+from fastapi.responses import JSONResponse 
 import motor.motor_asyncio 
 app = FastAPI() 
 # Connect to Mongo Atlas 
@@ -26,6 +28,31 @@ async def upload_audio(file: UploadFile = File(...)):
 
 @app.post("/player_score") 
 async def add_score(score: PlayerScore): 
-    score_doc = score.dict() 
+    score_doc = score.dict()
     result = await db.scores.insert_one(score_doc) 
     return {"message": "Score recorded", "id": str(result.inserted_id)} 
+
+def convert_id(document):
+    document["_id"] = str(document["_id"])
+    return document
+
+@app.get("/sprites")
+async def get_sprites():
+    sprites = []
+    async for sprite in db.sprites.find():
+        sprites.append(convert_id(sprite))
+    return sprites
+
+@app.get("/audios")
+async def get_audios():
+    audios = []
+    async for audio in db.audio.find():
+        audios.append(convert_id(audio))
+    return audios
+
+@app.get("/scores")
+async def get_scores():
+    scores = []
+    async for score in db.scores.find():
+        scores.append(convert_id(score))
+    return scores
